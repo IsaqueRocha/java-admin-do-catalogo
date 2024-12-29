7a4b66b3660c507d2baf497c1ee0e4884a394c6b
Isaque Rocha <isaquerocha@gmail.com>
2024-12-29 20:57:57 -0300

    feat: add category gateway

---
+++ domain/src/main/java/com/isaque/admin/catalogo/domain/category/CategoryGateway.java
@@ -0,0 +1,13 @@
+package com.isaque.admin.catalogo.domain.category;
+
+import com.isaque.admin.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+    Category create(Category category);
+    void deleteById(CategoryID id);
+    Optional<Category> findById(CategoryID id);
+    Category update(Category category);
+    Pagination<Category> findAll(CategorySearchQuery query);
+}
