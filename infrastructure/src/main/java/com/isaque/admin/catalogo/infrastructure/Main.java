e605c4d7a0024e4eb9d2921abf840b939ce98ba7
Isaque Rocha <isaquerocha@gmail.com>
2025-01-08 08:06:09 -0300

    chore: add springboot 3.4.1

--- infrastructure/src/main/java/com/isaque/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/isaque/admin/catalogo/infrastructure/Main.java
@@ -1,7 +1,13 @@
 package com.isaque.admin.catalogo.infrastructure;
 
+import com.isaque.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/isaque/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package com.isaque.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.isaque.admin.catalogo")
+public class WebServerConfig {
+
+}
