493af8cda293b406a1864c7cd5b5e5d366cdc759
Isaque Rocha <isaquerocha@gmail.com>
2025-02-15 14:39:48 -0300

    feat: add category controller

---
+++ infrastructure/src/main/java/com/isaque/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,25 @@
+package com.isaque.admin.catalogo.infrastructure.api.controllers;
+
+import com.isaque.admin.catalogo.domain.pagination.Pagination;
+import com.isaque.admin.catalogo.infrastructure.api.CategoryAPI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(
+            final String search,
+            final int page,
+            final int perPage,
+            final String sort,
+            final String direction
+    ) {
+        return null;
+    }
+}
