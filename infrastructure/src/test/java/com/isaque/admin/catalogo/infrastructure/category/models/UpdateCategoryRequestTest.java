960c097bd7540c212f03382b6da35ef0a91e5a92
Isaque Rocha <isaquerocha@gmail.com>
2025-03-01 20:11:03 -0300

    test: add update category request unmarshall test

---
+++ infrastructure/src/test/java/com/isaque/admin/catalogo/infrastructure/category/models/UpdateCategoryRequestTest.java
@@ -0,0 +1,40 @@
+package com.isaque.admin.catalogo.infrastructure.category.models;
+
+import com.isaque.admin.catalogo.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@JacksonTest
+class UpdateCategoryRequestTest {
+    @Autowired
+    private JacksonTester<CategoryResponse> json;
+
+    @Test
+    void testUnmarshall() throws IOException {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+        final var jsonString = """
+                {
+                    "name": "%s",
+                    "description": "%s",
+                    "is_active": "%s"
+                }
+                """.formatted(
+                expectedName,
+                expectedDescription,
+                expectedIsActive);
+
+        final var actualJson = this.json.parse(jsonString);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive);
+    }
+}
